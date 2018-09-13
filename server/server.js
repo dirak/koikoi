4efc013d0332f3bc747cc96248fb76ae528340b3
dirak <dirak.social@gmail.com>
2018-09-12 21:07:26 -0400

    socket io with client

--- server/server.js
+++ server/server.js
@@ -1,9 +1,21 @@
 const http = require('http')
 const express = require('express')
+const socketio = require('socket.io')
 
-const app = express()
-const server = http.createServer(app)
 const port = 8888
+const clientpath = `${__dirname}/../client`
+const app = express()
+
+app.use(express.static(clientpath))//tell our app where the files to serve exist
+console.log("Serving files from:", clientpath)
+
+const server = http.createServer(app)//create the server, this will help us handle http requests
+
+const io = socketio(server)//communication with the client
+
+io.on('connection', (socket) => {
+	socket.emit('message', 'Data being sent')
+})
 
 server.on('error', (err) => {
 	console.error('Server error:', err)
